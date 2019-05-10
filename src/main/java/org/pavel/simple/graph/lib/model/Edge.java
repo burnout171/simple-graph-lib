5c09bf5d200d809f24ea5a53be22d81bf10d8e71
Pavel Likin <pavellikin@gmail.com>
2019-05-10 17:44:04 +0300

    Remove lombok.

--- src/main/java/org/pavel/simple/graph/lib/model/Edge.java
+++ src/main/java/org/pavel/simple/graph/lib/model/Edge.java
@@ -1,17 +1,40 @@
 package org.pavel.simple.graph.lib.model;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.util.Objects;
 
-@Data
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Edge<V> {
 
     private final V source;
     private final V destination;
 
+    private Edge(V source, V destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+
     public static <V> Edge<V> of(V source, V destination) {
         return new Edge<>(source, destination);
+    }
+
+    public V getSource() {
+        return source;
+    }
+
+    public V getDestination() {
+        return destination;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Edge<?> edge = (Edge<?>) o;
+        return Objects.equals(source, edge.source) &&
+                Objects.equals(destination, edge.destination);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, destination);
     }
 }
--- src/main/java/org/pavel/simple/graph/lib/model/Vertex.java
+++ src/main/java/org/pavel/simple/graph/lib/model/Vertex.java
@@ -1,16 +1,33 @@
 package org.pavel.simple.graph.lib.model;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.util.Objects;
 
-@Data
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Vertex {
 
     private final String name;
 
+    private Vertex(String name) {
+        this.name = name;
+    }
+
     public static Vertex of(String name) {
         return new Vertex(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return Objects.equals(name, vertex.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
